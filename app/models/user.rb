d681807a1975e45969ebcbe7316b4621018c804b
Hina <hinachuhdry10@gmail.com>
2024-12-24 19:18:31 +0500

    Added devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  # Devise modules
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Validations
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :contact, presence: true, format: { with: /\A\d{10,15}\z/, message: "must be a valid phone number with 10-15 digits" }
+
+  # Helper Methods
+  before_save :set_full_name
+
+  def set_full_name
+    self.full_name = "#{first_name} #{last_name}"
+  end
+
+  def display_name
+    full_name.presence || email
+  end
 end
