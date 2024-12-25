f10ad4fe60374b50b8bceb3dcb76985281b4b3b3
Umar Farooq <umar.dev4973@gmail.com>
2024-12-26 04:42:06 +0500

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # You can replace '*' with your frontend's URL (e.g., 'http://localhost:3000' or 'https://mysite.com')
+    origins 'https://coursework-frontend-hqri-m242b6czd-hinas-projects-c12fa15f.vercel.app'  # Replace this with your frontend's URL
 
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options],
-             credentials: true # Allow credentials (like cookies or authorization headers) if needed
+             credentials: true # Allow credentials (cookies or authorization headers)
   end
 end
