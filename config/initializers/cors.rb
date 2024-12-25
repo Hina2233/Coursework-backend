e6942fd2d07b2460b5b208776a529fa44faaba83
Umar Farooq <umar.dev4973@gmail.com>
2024-12-26 04:46:23 +0500

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://coursework-frontend-hqri-m242b6czd-hinas-projects-c12fa15f.vercel.app'  # Replace this with your frontend's URL
+    origins 'https://coursework-frontend-hqri-m242b6czd-hinas-projects-c12fa15f.vercel.app', 'http://localhost:3000'  # Replace this with your frontend's URL
 
     resource '*',
              headers: :any,
