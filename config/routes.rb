3051739b44e00d917b976a22a6f271fca7977c74
Hina <hinachuhdry10@gmail.com>
2024-12-26 16:52:33 +0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
       root to: "users#index"
   end
 
+  get '/', to: "users#index"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
